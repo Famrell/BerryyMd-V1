40ea2d7a1e1b58b46d62b58039dc7f5bfb2e38e4
Famrell <100285618+Famrell@users.noreply.github.com>
2022-04-01 16:35:18 +0700

    Update broadcast.js

--- plugins/broadcast.js
+++ plugins/broadcast.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
     let time = require('moment-timezone').tz('Asia/Jakarta').format('HH:mm:ss')
-    let logo = fs.readFileSync('./src/elyas.jpg')
+    let logo = fs.readFileSync('./media/elyas.jpg')
     let chats = conn.chats.all().filter(v => !v.read_only && v.message).map(v => v.jid)
     conn.reply(m.chat, `_Send a broadcast message to ${chats.length} chats_\nestimation complete ${chats.length * 1.5} seconds`, m)
     for (let id of chats) {
