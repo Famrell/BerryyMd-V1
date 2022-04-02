4688c06da1fd482a08f7b72cab9bba10f7631329
Famrell <100285618+Famrell@users.noreply.github.com>
2022-04-02 19:35:02 +0700

    Create groupofc.js

---
+++ plugins/groupofc.js
@@ -0,0 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Grup Berryy')).buffer(), `
+「 Grup Berryy 」
+Grup 1:
+https://chat.whatsapp.com/KAHbnN7sCkrGghoVK9NjPX
+
+Grup 2:
+-
+Kalau linknya gak valid/direset hubungi owner
+Be A Smart People
+`.trim(), footer, 'Donasi', '.donasi', m)
+handler.help = ['grupbot']
+handler.tags = ['info']
+handler.command = /^(gcbot|grupbot|groupbot)$/i
+
+module.exports = handler
